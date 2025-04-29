ed3fe64cfd71023c8f813915ea4bc5a8cccf6ee8
Alex Mirror <example@dot.com>
2025-04-29 13:57:05 +0200

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -5,8 +5,40 @@
  *
  * @return {object}
  */
-function convertToObject(sourceString) {
-  // write your code here
+function convertToObject(styleString) {
+  if (typeof styleString !== 'string') {
+    return {};
+  }
+
+  const result = {};
+  const lines = styleString
+    .split(';')
+    .map((line) => line.replace(/\t/g, '').trim())
+    .filter(Boolean);
+
+  let currentProperty = null;
+  let currentValue = '';
+
+  for (const line of lines) {
+    if (line.includes(':')) {
+      if (currentProperty !== null) {
+        result[currentProperty] = currentValue;
+      }
+
+      const [prop, ...valueParts] = line.split(':');
+
+      currentProperty = prop.trim();
+      currentValue = valueParts.join(':').trim();
+    } else if (currentProperty !== null) {
+      currentValue += ';\n' + line;
+    }
+  }
+
+  if (currentProperty !== null) {
+    result[currentProperty] = currentValue;
+  }
+
+  return result;
 }
 
 module.exports = convertToObject;
